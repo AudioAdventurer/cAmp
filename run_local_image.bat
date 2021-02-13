46e698414e240756778f980ca4a14b685c88571e
Brian Nelson <brian@treegecko.com>
2021-02-13 08:45:12 -0800

    break out push from build

---
+++ run_local_image.bat
@@ -0,0 +1,18 @@
+REM Stop image if running
+docker stop cAmp
+
+REM Remove existing
+docker rm cAmp
+
+REM Run the new image
+REM Adjust your media locations as needed (c:\Media\Data, c:\Media\Music)
+REM The data folder is where the database will be created to track playlists and history
+docker run -d  ^
+		   -p 8080:8000 ^
+		   -e "CAMP_MUSIC_FOLDER=/Media/Music" ^
+		   -e "CAMP_DATA_FOLDER=/Media/Data" ^
+		   -v c:\Media\Data:/Media/Data ^
+		   -v c:\Media\Music:/Media/Music ^
+		   --restart unless-stopped ^
+		   --name=cAmp ^
+		   "audioadventurer/camp:latest"
